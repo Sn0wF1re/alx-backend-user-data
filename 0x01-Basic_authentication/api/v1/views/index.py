7aa818e9bff0b9ccc3b82f48835c800268a6aa7a
Sn0wF1re <mokayaclinton34@gmail.com>
2023-05-29 17:13:54 +0300

    Create error handler for forbidden status

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -33,3 +33,11 @@ def not_authorized() -> str:
   Raise 401 error
   """
   abort(401, description="Unauthorized")
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+  """
+  Raise 403 error
+  """
+  abort(403, description="Forbidden")
