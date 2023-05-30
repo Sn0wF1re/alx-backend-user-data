ec6665dfe101c8820efce4badb2afe012207769c
Sn0wF1re <mokayaclinton34@gmail.com>
2023-05-30 17:41:30 +0300

    Improve method to allow password with ':'

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -53,7 +53,7 @@ class BasicAuth(Auth):
                 or not isinstance(decoded_base64_authorization_header, str) \
                 or ':' not in decoded_base64_authorization_header:
             return (None, None)
-        data = decoded_base64_authorization_header.split(':')
+        data = decoded_base64_authorization_header.split(':', 1)
         return (data[0], data[1])
 
     def user_object_from_credentials(
