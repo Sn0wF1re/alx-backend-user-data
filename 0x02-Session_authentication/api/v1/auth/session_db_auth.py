83be5d9e7d053f2069bef84615bb4774ad57344d
Sn0wF1re <mokayaclinton34@gmail.com>
2023-06-03 13:21:58 +0300

    Edit class

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -4,7 +4,7 @@ Create class SessionDBAuth
 """
 from .session_exp_auth import SessionExpAuth
 from models.user_session import UserSession
-from flask import request
+from datetime import datetime, timedelta
 
 
 class SessionDBAuth(SessionExpAuth):
@@ -15,16 +15,14 @@ class SessionDBAuth(SessionExpAuth):
         """
         Create session id
         """
-        try:
-            session_id = super().create_session(user_id)
-        except Exception:
-            return
-        session = UserSession(**{
+        session_id = super().create_session(user_id)
+        if isinstance(session_id, str):
+            session = UserSession(**{
                 'user_id': user_id,
                 'session_id': session_id
-        })
-        session.save()
-        return session_id
+            })
+            session.save()
+            return session_id
 
     def user_id_for_session_id(self, session_id=None):
         """
@@ -36,6 +34,10 @@ class SessionDBAuth(SessionExpAuth):
             return None
 
         if len(sessions) <= 0:
+            return None
+        now = datetime.now()
+        duration = timedelta(seconds=self.session_duration)
+        if sessions[0].created_at + duration < now:
             return None
         return sessions[0].user_id
 
