ac06ef8cb7ab7ce1bd65a0f40cdce430aefbda7b
Sn0wF1re <mokayaclinton34@gmail.com>
2023-06-06 14:46:46 +0300

    Create method to register new user in database

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,6 +3,9 @@
 define a _hash_password method
 """
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
@@ -11,3 +14,24 @@ def _hash_password(password: str) -> bytes:
     hashed with bcrypt.hashpw
     """
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        should take mandatory email
+        and password string arguments and return a User object
+        """
+        try:
+            self._db.find_user_by(email=email)
+        except NoResultFound:
+            return self._db.add_user(
+                email=email, hashed_password=_hash_password(password)
+            )
+        raise ValueError(f"User {email} already exists")
