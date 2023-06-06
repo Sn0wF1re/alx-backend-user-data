70d68399612e1ece4fede38a1eaf549b9a259541
Sn0wF1re <mokayaclinton34@gmail.com>
2023-06-06 23:09:04 +0300

    Find user based on email and update their reset token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -92,3 +92,16 @@ class Auth:
         except ValueError:
             return None
         return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """
+        update the user's reset_token database field for a user.
+        Return the token.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+        token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=token)
+        return token
